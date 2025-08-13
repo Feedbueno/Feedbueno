4f2fe676347b7c672f692bd4db959a2746b7a4b8
Feedbueno <omrey86@gmail.com>
2025-08-13 20:38:28 +0200

    Last Sync: 2025-08-13 20:38 (Mobile)

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
+// pages/index.js
 export default function Home() {
-  return (
-    <div>
-      <h1>Feeds Project</h1>
-      <p>Este sitio sirve feeds en /imetal/feed.xml y /isouls/feed.xml</p>
-    </div>
-  );
+  if (typeof window !== 'undefined') {
+    window.location.href = '/index.html';
+  }
+  return null;
 }
